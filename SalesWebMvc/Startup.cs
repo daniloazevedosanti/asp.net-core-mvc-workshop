eb137d122cd726771b80c95f2e9cfc1e56298f4a
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2019-03-10 15:08:43 -0300

    App locale, number and date formatting

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,61 +18,72 @@ using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+   public class Startup
+   {
+      public Startup(IConfiguration configuration)
+      {
+         Configuration = configuration;
+      }
 
-        public IConfiguration Configuration { get; }
+      public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+      // This method gets called by the runtime. Use this method to add services to the container.
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.Configure<CookiePolicyOptions>(options =>
+         {
+               // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+               options.CheckConsentNeeded = context => true;
+            options.MinimumSameSitePolicy = SameSiteMode.None;
+         });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+         services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
-                        builder.MigrationsAssembly("SalesWebMvc")));
+         services.AddDbContext<SalesWebMvcContext>(options =>
+                 options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                     builder.MigrationsAssembly("SalesWebMvc")));
 
-            services.AddScoped<SeedingService>();
-            services.AddScoped<SellerServices>();
-            services.AddScoped<DepartmentService>();
-        }
+         services.AddScoped<SeedingService>();
+         services.AddScoped<SellerServices>();
+         services.AddScoped<DepartmentService>();
+      }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                seedingService.Seed();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
+      {
+         var enUS = new CultureInfo("en-US");
+         var localizationOption = new RequestLocalizationOptions
+         {
+            DefaultRequestCulture = new RequestCulture(enUS),
+            SupportedCultures = new List<CultureInfo> { enUS },
+            SupportedUICultures = new List<CultureInfo> { enUS }
+         };
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+         app.UseRequestLocalization(localizationOption);
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+
+         if (env.IsDevelopment())
+         {
+            app.UseDeveloperExceptionPage();
+            seedingService.Seed();
+         }
+         else
+         {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+         }
+
+         app.UseHttpsRedirection();
+         app.UseStaticFiles();
+         app.UseCookiePolicy();
+
+         app.UseMvc(routes =>
+         {
+            routes.MapRoute(
+                   name: "default",
+                   template: "{controller=Home}/{action=Index}/{id?}");
+         });
+      }
+   }
 }
