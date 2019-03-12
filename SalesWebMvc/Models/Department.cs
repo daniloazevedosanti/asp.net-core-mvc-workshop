9f2018324dfd865f5dce611e6c4c7640c8e4b381
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2019-03-11 21:14:32 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,32 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models
 {
-    public class Department
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Seller> Seller { get; set; } = new List<Seller>();
+   public class Department
+   {
+      public int Id { get; set; }
+
+      [Required(ErrorMessage = "{0} of department required")]
+      public string Name { get; set; }
+
+      public ICollection<Seller> Seller { get; set; } = new List<Seller>();
 
 
-        public Department() { }
+      public Department() { }
 
-        public Department(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
+      public Department(int id, string name)
+      {
+         Id = id;
+         Name = name;
+      }
 
-        public void AddSeller(Seller seller)
-        {
-            Seller.Add(seller);
-        }
+      public void AddSeller(Seller seller)
+      {
+         Seller.Add(seller);
+      }
 
-        public double totalSales (DateTime initial, DateTime final)
-        {
-            return Seller.Sum(seller => seller.TotalSales( initial, final));
-        }
-    }
+      public double totalSales(DateTime initial, DateTime final)
+      {
+         return Seller.Sum(seller => seller.TotalSales(initial, final));
+      }
+   }
 }
--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -1,23 +1,30 @@
 ﻿using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
-    public class DepartmentService
-    {
-        private readonly SalesWebMvcContext _context;
+   public class DepartmentService
+   {
+      private readonly SalesWebMvcContext _context;
 
-        public DepartmentService (SalesWebMvcContext context)
-        {
-            _context = context;
-        }
+      public DepartmentService(SalesWebMvcContext context)
+      {
+         _context = context;
+      }
 
-        public List<Department> FindAll()
-        {
-            return _context.Department.OrderBy(x => x.Name).ToList();
-        }
+      public List<Department> FindAll()
+      {
+         return _context.Department.OrderBy(x => x.Name).ToList();
+      }
+
+      public async Task<List<Department>> FindAllAsync()
+      {
+         return await _context.Department.OrderBy(x => x.Name).ToListAsync();
+      }
 
 
-    }
+   }
 }
